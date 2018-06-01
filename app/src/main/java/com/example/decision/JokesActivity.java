e8204638e65d5a9a6acdb27a7ed90f21bd048a32
moti <motibahar10@gmail.com>
2018-06-01 20:31:44 +0300

    Jokes activity

--- app/src/main/java/com/example/decision/JokesActivity.java
+++ app/src/main/java/com/example/decision/JokesActivity.java
@@ -1,13 +1,37 @@
 package com.example.decision;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class JokesActivity extends AppCompatActivity {
+
+    private boolean isJokes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokes);
+
+        Intent intent = getIntent();
+        isJokes = intent.getBooleanExtra(Utils.EXTRA_IS_JOKES, false);
+
+        TextView myAwesomeTextView = (TextView)findViewById(R.id.joke_or_fact);
+
+
+        Random random = new Random();
+        int randomNum = random.nextInt(Utils.JOKES.length);
+        if(isJokes == true){
+            myAwesomeTextView.setText(Utils.JOKES[randomNum]);
+//            myAwesomeTextView.setText(Utils.JOKE5);
+        }else{
+            myAwesomeTextView.setText(Utils.FACTS[randomNum]);
+        }
+        myAwesomeTextView.setMovementMethod(new ScrollingMovementMethod());
+
     }
 }
--- app/src/main/java/com/example/decision/Utils.java
+++ app/src/main/java/com/example/decision/Utils.java
@@ -16,4 +16,90 @@ import java.util.List;
 
 public class Utils {
   final static public String EXTRA_IS_JOKES = "IS_JOKES";
+  final static public String JOKE1 = "השארתי לו רשימת קניות על המקרר\n" +
+          "\n" +
+          "עוף\n" +
+          "\n" +
+          "מפה\n" +
+          "\n" +
+          "שמן\n" +
+          "\n" +
+          "בטטה\n" +
+          "\n" +
+          "הוא עזב בלי להגיד מילה\n";
+  final static public String JOKE2 = "אמא שלי איימה עלי שאם אעשה עוד טעות אחת היא תמכור אותי.\n" +
+          " תקנו אותי אם אני טועה.\n";
+  final static public String JOKE3 = "*מתעטש ליד אמא* \n" +
+          "מה אתה חולה ? להכין לך מרק עוף ? להכין לך תה ? אולי לא תלך לבית ספר ?\n" +
+          "\n" +
+          "*מתעטש ליד אבא*\n" +
+          "לבריאות אור\n" +
+          "אבא אני יהלי...\n";
+  final static public String JOKE4 = "איך לזהות עצלנים , חוק מס' 3523622362 :\n" +
+          "נכון שהתעצלת לקרוא את המספר ?\n";
+  final static public String JOKE5 = "3 אנשים יושבים בבית יולדות.\n" +
+          "היולדת יוצאת ואומרת לאחד מזל טוב יש לך 7 תינוקות!\n" +
+          "האיש אומר איזה צירוף מקרים אני גר בבאר שבע.\n" +
+          "היולדת יוצאת לאיש השני ואומרת מזל טוב יש לך 8 תינוקות!\n" +
+          "האיש אומר איזה צירוף מקרים אני גר בקרית שמונה.\n" +
+          "היולדת יוצאת לאיש השלישי ורואה אותו בורח במהירות. \n" +
+          "היא רצה אחריו ושואלת אותו למה אתה בורח?\n" +
+          "האיש עונה לה \"אני גר באלפי מנשה!!!\"\n";
+  final static public String JOKE6 = "שיכור ופוזל התנגשו ברחוב ונפלו.\n" +
+          "הפוזל: \"אולי תסתכל לאן שאתה הולך?!\"\n" +
+          "השיכור: \"אולי תלך לאן שאתה מסתכל?!\"\n";
+  final static public String JOKE7 = "3";
+  final static public String JOKE8 = "3";
+  final static public String JOKE9 = "3";
+  final static public String JOKE10 = "3";
+  final static public String JOKE11 = "3";
+  final static public String JOKE12 = "3";
+  final static public String JOKE13 = "3";
+  final static public String JOKE14 = "3";
+  final static public String JOKE15 = "3";
+  final static public String JOKE16 = "3";
+  final static public String JOKE17 = "3";
+  final static public String JOKE18 = "3";
+  final static public String JOKE19 = "3";
+  final static public String JOKE20 = "3";
+  final static public String JOKE21 = "3";
+  final static public String JOKE22 = "3";
+
+
+
+
+
+  final static public String FACT1 = "3";
+  final static public String FACT2 = "3";
+  final static public String FACT3 = "3";
+  final static public String FACT4 = "3";
+  final static public String FACT5 = "3";
+  final static public String FACT6 = "3";
+  final static public String FACT7 = "3";
+  final static public String FACT8 = "3";
+  final static public String FACT9 = "3";
+  final static public String FACT10 = "3";
+  final static public String FACT11 = "3";
+  final static public String FACT12 = "3";
+  final static public String FACT13 = "3";
+  final static public String FACT14 = "3";
+  final static public String FACT15 = "3";
+  final static public String FACT16 = "3";
+  final static public String FACT17 = "3";
+  final static public String FACT18 = "3";
+  final static public String FACT19 = "3";
+  final static public String FACT20 = "3";
+  final static public String FACT21 = "3";
+  final static public String FACT22 = "3";
+
+  final static public String[] JOKES =
+          {JOKE1, JOKE2, JOKE3, JOKE4, JOKE5, JOKE6, JOKE7, JOKE8,
+                  JOKE9, JOKE10, JOKE11, JOKE12, JOKE13, JOKE14, JOKE15, JOKE16,
+                  JOKE17, JOKE18, JOKE19, JOKE20, JOKE21, JOKE22};
+
+
+  final static public String[] FACTS =
+          {FACT1, FACT2, FACT3, FACT4, FACT5, FACT6, FACT7, FACT8,
+                  FACT9, FACT10, FACT11, FACT12, FACT13, FACT14, FACT15, FACT16,
+                  FACT17, FACT18, FACT19, FACT20, FACT21, FACT22};
 }
